fc672d477f37690438cbe543762bb231bc17604d
Amir <krifa.amir@gmail.com>
2015-07-13 20:50:16 +0200

    Update UnShortenUrl.py
    Support queries timeout.

--- UnShortenUrl.py
+++ UnShortenUrl.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-
+TIMEOUT = 10
 class UnShortenUrl:
     def process(self, url, previous_url=None):
+        logging.info('Init url: %s'%url)
         import urlparse
         import httplib
         try:
             parsed = urlparse.urlparse(url)
             if parsed.scheme == 'https':
-                h = httplib.HTTPSConnection(parsed.netloc)
+                h = httplib.HTTPSConnection(parsed.netloc, timeout=TIMEOUT)
             else:
-                h = httplib.HTTPConnection(parsed.netloc)
+                h = httplib.HTTPConnection(parsed.netloc, timeout=TIMEOUT)
             resource = parsed.path
             if parsed.query != "": 
                 resource += "?" + parsed.query
-            h.request('HEAD', resource )
-            response = h.getresponse()
+            h.request('HEAD', 
+                      resource, 
+                      headers={'User-Agent': 'curl/7.38.0'#'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36'}
+                               }
+                      )
+            try:
+                response = h.getresponse()
+            except:
+                logging.error('Timeout for url: %s'%(url))
+                raise
+            logging.info('Response status: %d'%response.status)
             if response.status/100 == 3 and response.getheader('Location'):
                 red_url = response.getheader('Location')
+                logging.info('Red, previous: %s, %s'%(red_url, previous_url))
                 if red_url == previous_url:
                     return red_url
                 return self.process(red_url, previous_url=url) 
